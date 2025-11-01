e1c08a0dbdcd822d5704d494ec556dc7f2fe05e6
h4r5h1l <h4r5h1l@gmail.com>
2025-11-01 10:59:35 -0400

    B: Completed until Collisions.

---
+++ circleshape.py
@@ -0,0 +1,29 @@
+import pygame
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite): #extends Sprite Class
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # sub-classes must override
+        pass
+
+    def update(self, dt):
+        # sub-classes must override
+        pass
+    
+    def collision_check(self, obj):
+        if obj.position.distance_to(self.position) <= (obj.radius + self.radius):
+            return True
+        else:
+            return False
+        
--- main.py
+++ main.py
@@ -1,22 +1,44 @@
 import pygame
+from player import Player
 from constants import *
-
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-def game_loop():
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    asteroids_group = pygame.sprite.Group()
+    Player.containers = (updatable_group, drawable_group)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    Asteroid.containers = (asteroids_group, updatable_group, drawable_group)
+    AsteroidField.containers = updatable_group
+    asteroidfield = AsteroidField()
+    
+    # Game Loop
     while True:
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for obj in drawable_group:
+            obj.draw(screen)
+        updatable_group.update(dt)
         pygame.display.flip()
-
-def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    game_loop()
+        
+        for obj in asteroids_group:
+            if obj.collision_check(player):
+                print("Game Over!")
+                sys.exit()
+        
+        
 
 
 if __name__ == "__main__":
